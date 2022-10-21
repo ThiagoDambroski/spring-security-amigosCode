92a40a2bed10bec9a5efaabe3dc346cfbec27e2c
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-21 09:42:53 -0300

    Initial commit

---
+++ src/main/java/com/dambroski/springsecurityamigosCode/SpringSecurityAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.dambroski.springsecurityamigosCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAmigosCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dambroski/springsecurityamigosCode/SpringSecurityAmigosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dambroski.springsecurityamigosCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAmigosCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
