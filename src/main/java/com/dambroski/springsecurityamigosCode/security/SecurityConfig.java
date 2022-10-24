d80949ddd6a680c4209f69da99204c7dcd77d12b
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-24 20:05:15 -0300

    In memory user details manager

--- src/main/java/com/dambroski/springsecurityamigosCode/security/SecurityConfig.java
+++ src/main/java/com/dambroski/springsecurityamigosCode/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.dambroski.springsecurityamigosCode.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +23,17 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.authenticated() // any request must be authenticated
 		.and()
 		.httpBasic();
+	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+	UserDetails leticiaUser = User.builder()
+			.username("leticia")
+			.password("linda")
+			.roles("STUDENT") //ROLE_STUDENT
+			.build();
+	
+	return new InMemoryUserDetailsManager(leticiaUser);
 	}
 }
