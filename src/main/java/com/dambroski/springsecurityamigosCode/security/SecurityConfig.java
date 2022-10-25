a34c101fe769c656aab3eea0174559e299e9779e
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-24 22:03:52 -0300

    Role based authentication

--- src/main/java/com/dambroski/springsecurityamigosCode/security/SecurityConfig.java
+++ src/main/java/com/dambroski/springsecurityamigosCode/security/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers("/", "index", "/css/*","/js/*")
-		.permitAll()
+		.antMatchers("/", "index", "/css/*","/js/*").permitAll()
+		.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
 		.anyRequest()
 		.authenticated() // any request must be authenticated
 		.and()
@@ -44,7 +44,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 			.build();
 	
 	UserDetails thiagoUser = User.builder()
-			.username("thaigo")
+			.username("thiago")
 			.password(passwordEncoder.encode("123"))
 			.roles(ApplicationUserRole.ADMIN.name())
 			.build();
